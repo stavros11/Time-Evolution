a8bf3d9b2d41c4dfd0849b5065be04dc221fa358
stavros11 <stavros11@users.noreply.github.com>
2019-07-13 12:22:59 +0300

    Add full MPS propagator

--- machines/mps_trotter.py
+++ machines/mps_trotter.py
@@ -1,8 +1,9 @@
 """Evolution using the MPO form of the Trotterized evolution operator."""
 
 import numpy as np
-from machines import mps_utils
+import scipy
 import utils
+from machines import mps_utils
 
 
 def apply_prodop(op, mps):
@@ -38,6 +39,21 @@ def apply_mpo(op, mps):
   return res.reshape(shape)
 
 
+def split_two_qubit(u12, n_sites, d_bond):
+  assert n_sites % 2 == 0
+  u12 = u12.reshape(4 * (2,)).transpose([0, 2, 1, 3])
+
+  u, s, v = np.linalg.svd(u12.reshape((4, 4)))
+  v = np.diag(s[:d_bond]).dot(v[:d_bond])
+
+  u = u[:, :d_bond].reshape((2, 2, d_bond)) # (d, d, D)
+  v = v.reshape((d_bond, 2, 2)) # (D, d, d)
+
+  even = np.einsum("lum,mdr->ludr", v, u)
+  odd = np.einsum("umr,lmd->ludr", u, v)
+  return np.stack((n_sites // 2) * [even, odd])
+
+
 def truncate_bond_dimension(mps, d_bond):
   """Truncates the MPS bond dimension to a smaller one using SVD.
 
@@ -67,9 +83,9 @@ def truncate_bond_dimension(mps, d_bond):
   return np.concatenate((boundary[np.newaxis], new), axis=0)
 
 
-class TFIMTrotterMPS:
+class TFIMBase:
 
-  def __init__(self, psi0, d_bond, dt, h=1.0):
+  def __init__(self, psi0, d_bond):
     self.dtype = psi0.dtype
     self.d_bond = d_bond
     self.pauli = utils.Pauli(dtype=self.dtype)
@@ -78,34 +94,8 @@ class TFIMTrotterMPS:
     self.tensors[0] = self.tensors[0].swapaxes(1, 2)
     self.n_sites = len(self.tensors[0])
 
-    self.x_op = self._construct_x_op(h, dt)
-    self.zz_op = self._construct_zz_op(dt)
-
-  def _construct_x_op(self, h, dt):
-    cos, sin = np.cos(h * dt), np.sin(h * dt)
-    exp_x = np.array([[cos, 1j * sin], [1j * sin, cos]], dtype=self.dtype)
-    return np.stack(self.n_sites * [exp_x])
-
-  def _construct_zz_op(self, dt):
-    assert self.n_sites % 2 == 0
-    exp = np.exp(1j * dt)
-    u12 = np.diag([exp, exp.conj(), exp.conj(), exp]).astype(self.dtype)
-    u12 = u12.reshape(4 * (2,)).transpose([0, 2, 1, 3])
-
-    u, s, v = np.linalg.svd(u12.reshape((4, 4)))
-    v = np.diag(s[:2]).dot(v[:2])
-
-    u = u[:, :2].reshape((2, 2, 2)) # (d, d, D)
-    v = v.reshape((2, 2, 2)) # (D, d, d)
-
-    even = np.einsum("lum,mdr->ludr", v, u)
-    odd = np.einsum("umr,lmd->ludr", u, v)
-    return np.stack((self.n_sites // 2) * [even, odd])
-
-  def evolution_step(self, mps0):
-    zz_evolved = apply_mpo(self.zz_op, mps0)
-    zz_trunc = truncate_bond_dimension(zz_evolved, self.d_bond)
-    return apply_prodop(self.x_op, zz_trunc)
+  def evolution_step():
+    raise NotImplementedError
 
   def evolve(self, time_steps):
     for i in range(time_steps):
@@ -117,3 +107,41 @@ class TFIMTrotterMPS:
       self.tensors = self.evolve(time_steps)
     assert len(self.tensors) == time_steps + 1
     return mps_utils.mps_to_dense(self.tensors.swapaxes(0, 1))
+
+
+class TFIMTrotter(TFIMBase):
+
+  def __init__(self, psi0, d_bond, dt, h=1.0):
+    super().__init__(psi0, d_bond)
+    self.x_op = self._construct_x_op(h, dt)
+    self.zz_op = self._construct_zz_op(dt)
+
+  def _construct_x_op(self, h, dt):
+    cos, sin = np.cos(h * dt), np.sin(h * dt)
+    exp_x = np.array([[cos, 1j * sin], [1j * sin, cos]], dtype=self.dtype)
+    return np.stack(self.n_sites * [exp_x])
+
+  def _construct_zz_op(self, dt):
+    exp = np.exp(1j * dt)
+    u12 = np.diag([exp, exp.conj(), exp.conj(), exp]).astype(self.dtype)
+    return split_two_qubit(u12, self.n_sites, d_bond=2)
+
+  def evolution_step(self, mps0):
+    zz_evolved = apply_mpo(self.zz_op, mps0)
+    zz_trunc = truncate_bond_dimension(zz_evolved, self.d_bond)
+    return apply_prodop(self.x_op, zz_trunc)
+
+
+class TFIMFull(TFIMBase):
+
+  def __init__(self, psi0, d_bond, dt, h=1.0):
+    super().__init__(psi0, d_bond)
+    pauli = utils.Pauli(self.dtype)
+    ham12 = -np.kron(pauli.Z, pauli.Z)
+    ham12 += -h * (np.kron(pauli.I, pauli.X) + np.kron(pauli.X, pauli.I))
+    u12 = scipy.linalg.expm(-1j * dt * ham12)
+    self.op = split_two_qubit(u12, self.n_sites, d_bond=4)
+
+  def evolution_step(self, mps0):
+    evolved = apply_mpo(self.op, mps0)
+    return truncate_bond_dimension(evolved, self.d_bond)
