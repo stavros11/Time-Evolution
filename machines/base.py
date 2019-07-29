05cc36f6a3cd8c9c8d1ec21ac36d3ced566ff8c3
stavros11 <35475381+stavros11@users.noreply.github.com>
2019-07-29 12:23:04 +0200

    Add t-VMC machine documentation

--- machines/base.py
+++ machines/base.py
@@ -5,6 +5,7 @@ All machines should inherit this.
 
 
 class BaseMachine:
+  """Base machine to use with Clock optimization."""
 
   def __init__(self, n_sites, time_steps):
     # Time steps do not include initial condition
@@ -20,6 +21,9 @@ class BaseMachine:
 
     This is used because the C++ sampler currently only supports full
     wavefunctions to avoid much communication with Python.
+
+    Returns:
+      dense form of the wavefunction with shape (M+1, Ns)
     """
     raise NotImplementedError
 
@@ -55,5 +59,33 @@ class BaseMachine:
 
     Args:
       to_add: Value to add to the variational parameters.
+    """
+    raise NotImplementedError
+
+
+class BaseStepMachine(BaseMachine):
+  """Base machine to use with traditional t-VMC.
+
+  Note that no machine inherits this class. It is only used for documentation
+  purposes.
+
+  Most methods are the same as above with the following diferences:
+    `dense` returns shape (2^N).
+    `wavefunction` takes only `configs` (not `times`) and
+      returns shape (Ns,).
+    `gradient` takes only `configs` (not `times`).
+  """
+
+  def __init__(self, n_sites):
+    # __init__ should define the following attributes
+    self.n_sites = n_sites
+    self.dtype = None # Type of the variational parameters
+    self.shape = None # Shape of the variational parameters
+    self.name = None # Name (str) of the machine for saving purposes
+
+  def update(self, s_matrix, f_vector):
+    """Evolves variational parameters one time step.
+
+    Uses Runge-Kutta integration.
     """
     raise NotImplementedError
