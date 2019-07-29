d94347c11cb38cf5f45a661a8e0f01ffb18d3d32
stavros11 <stavros11@users.noreply.github.com>
2019-07-29 21:32:18 +0200

    Add MC sampling option

--- np_tvmc.py
+++ np_tvmc.py
@@ -3,21 +3,37 @@
 Uses all states to calculate gradients.
 """
 
-#import h5py
+import os
+import ctypes
+import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 import utils
 from energy import tvmc
 from machines import mps
+from numpy.ctypeslib import ndpointer
 
 
-n_sites = 4
+# Model parameters
+n_sites = 6
 time_steps = 20
 t_final = 1.0
 h_init = 1.0
 h_ev = 0.5
 n_epochs = 10000
 n_message = 500
+bond_list = [2, 3, 4, 6]
+
+# Sampling parameters (per time when using the space only sampler)
+sampler = ctypes.CDLL(os.path.join(os.getcwd(), "samplers", "spacevmclib.so"))
+sampler.run.argtypes = ([ndpointer(np.complex128, flags="C_CONTIGUOUS")] +
+                         6 * [ctypes.c_int] +
+                         [ndpointer(ctypes.c_int, flags="C_CONTIGUOUS")])
+sampler.run.restype = None
+sampler.n_samples = 5000
+sampler.n_corr = 1
+sampler.n_burn = 10
+
 
 t_grid = np.linspace(0.0, t_final, time_steps + 1)
 dt = t_grid[1] - t_grid[0]
@@ -27,17 +43,36 @@ ham = utils.tfim_hamiltonian(n_sites, h=h_ev)
 exact_state, obs = utils.tfim_exact_evolution(n_sites, t_final, time_steps,
                                               h0=h_init, h=h_ev)
 
-# Initialize machine
-machine = mps.SmallMPSStepMachine(exact_state[0], d_bond=5)
 
-history = {"overlaps" : [], "avg_overlaps": [], "exact_Eloc": []}
-full_psi = [machine.dense()]
-for step in range(time_steps):
-  rhs, Ok, Ok_star_Eloc, Eloc, Ok_star_Ok = tvmc.exact_tvmc_step(machine,
-                                                                 h=h_ev)
+history = {"overlaps" : [], "avg_overlaps": []}
+for d_bond in bond_list:
+  machine = mps.SmallMPSStepMachine(exact_state[0], d_bond=d_bond)
+  full_psi = tvmc.evolve(machine, time_steps, dt, h=h_ev, sampler=sampler)
 
-  machine.update(-1j * rhs * dt)
-  full_psi.append(machine.dense())
+  history["overlaps"].append(utils.overlap(full_psi, exact_state))
+  history["avg_overlaps"].append(utils.averaged_overlap(full_psi, exact_state))
 
-full_psi = np.array(full_psi)
-print(utils.averaged_overlap(full_psi, exact_state))
+  print("\nD: {}".format(d_bond))
+  print("Overlap: {}".format(history["overlaps"][-1]))
+  print("Averaged Overlap: {}".format(history["avg_overlaps"][-1]))
+
+  # Save dense
+  if sampler is None:
+    filename = "tvmc_allstates_{}_N{}M{}.npy".format(
+        machine.name, n_sites, time_steps)
+  else:
+    filename = "tvmc_sampling{}_{}_N{}M{}.npy".format(
+        sampler.n_samples, machine.name, n_sites, time_steps)
+  np.save("final_dense/{}".format(filename), full_psi)
+
+
+if sampler is None:
+  filename = "tvmc_allstates_mpsd{}_{}_N{}M{}".format(
+      bond_list[0], bond_list[-1], n_sites, time_steps)
+else:
+  filename = "tvmc_sampling{}_mpsd{}_{}_N{}M{}".format(
+      sampler.n_samples, bond_list[0], bond_list[-1], n_sites, time_steps)
+file = h5py.File("histories/{}.h5py".format(filename), "w")
+for k in history.keys():
+  file[k] = history[k]
+file.close()
