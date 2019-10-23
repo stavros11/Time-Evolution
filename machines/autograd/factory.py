5a41dcde8b345789641711a293cb1010f3149c11
stavros11 <35475381+stavros11@users.noreply.github.com>
2019-10-23 23:47:11 +0200

    Add autograd SmallMPS machine

--- machines/autograd/factory.py
+++ machines/autograd/factory.py
@@ -1,9 +1,11 @@
 """Import all implemented autograd machines."""
 
 from machines.autograd import full
+from machines.autograd import mps
 from machines.autograd import neural
 
 FullWavefunction = full.FullWavefunctionModel
 FullPropagator = full.FullPropagatorModel
+SmallMPS = mps.SmallMPSModel
 StepConv = neural.StepConvModel
 StepFeedForward = neural.StepFeedForwardModel
--- machines/autograd/mps.py
+++ machines/autograd/mps.py
@@ -1,21 +1,45 @@
 import numpy as np
 import tensorflow as tf
 from machines.autograd import base
+from typing import Tuple
 
 
 class SmallMPSModel(base.BaseAutoGrad):
 
   def __init__(self, **kwargs):
-    init_state = kwargs["init_state"]
     super(SmallMPSModel, self).__init__(**kwargs)
-    self.name = "fullwv_autograd"
+    self.name = "smallmps_autograd"
+    self.d_phys = 2
+    self.d_bond = 4
+    self.n_states = self.d_phys**self.n_sites
 
-    # Initialize full wavefunction by repeating the initial state
-    init_value = np.array((self.time_steps + 1) * [init_state]).ravel()
-    self.psi_re = self.add_variable(init_value.real)
-    self.psi_im = self.add_variable(init_value.imag)
+    boundary_tensor = np.zeros((self.time_steps, self.d_phys, self.d_bond))
+    boundary_tensor[:, 0, 0], boundary_tensor[:, 1, 0] = 1.0, 1.0
+    middle_tensor = np.array(self.time_steps * self.d_phys * [np.eye(self.d_bond)])
+    middle_tensor = middle_tensor.reshape(
+        (self.time_steps, self.d_phys, self.d_bond, self.d_bond))
+
+    self.tensors = [self._add_complex_variable_with_noise(boundary_tensor)]
+    for _ in range(1, self.n_sites - 1):
+      self.tensors.append(self._add_complex_variable_with_noise(middle_tensor))
+    self.tensors.append(self._add_complex_variable_with_noise(boundary_tensor))
+
+  def _add_complex_variable_with_noise(self, value, std=1e-3
+                                       ) -> Tuple[tf.Tensor, tf.Tensor]:
+    noise = np.random.normal(0.0, std, size=value.shape)
+    re = self.add_variable(value + noise)
+    noise = np.random.normal(0.0, std, size=value.shape)
+    im = self.add_variable(value + noise)
+    return re, im
 
   def forward(self) -> tf.Tensor:
-    psi = tf.complex(self.psi_re, self.psi_im)
-    psi = tf.reshape(psi, self.dense_shape)
-    return tf.concat([self.init_state, psi[1:]], axis=0)
+    re, im = self.tensors[0]
+    prod = tf.complex(re, im)
+    for re, im in self.tensors[1:-1]:
+      prod = tf.einsum("taj,tbjk->tabk", prod, tf.complex(re, im))
+      shape = list(prod.shape)
+      prod = tf.reshape(prod, (shape[0], shape[1] * shape[2], shape[3]))
+    re, im = self.tensors[-1]
+    prod = tf.einsum("taj,tbj->tab", prod, tf.complex(re, im))
+    psi = tf.reshape(prod, (self.time_steps, self.n_states))
+    return tf.concat([self.init_state, psi], axis=0)
