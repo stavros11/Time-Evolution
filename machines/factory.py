9dfcc4bc9b68352dd47299166a1822877691ad53
stavros11 <35475381+stavros11@users.noreply.github.com>
2019-11-08 15:56:48 +0100

    Fix bug with importing deleted module

--- machines/factory.py
+++ machines/factory.py
@@ -5,13 +5,8 @@ from machines.full import FullWavefunctionNormalized
 from machines.mps import SmallMPS
 from machines.mps import SmallMPSNormalized
 from optimization import deterministic
-from optimization import sweeping
 
 
 # Map machines to compatible gradient calculation functions
 machine_to_gradient_func = {"FullWavefunction": deterministic.gradient,
                             "SmallMPS": deterministic.sampling_gradient}
-
-# Map machines to compatible sweepers
-machine_to_sweeper = {"FullWavefunction": sweeping.ExactGMResSweep,
-                      "SmallMPS": sweeping.ExactGMResSweep}
