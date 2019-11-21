2c44ac2426b9b364e27f91b16bf8df2b242fc069
stavros11 <35475381+stavros11@users.noreply.github.com>
2019-11-21 21:51:43 +0100

    Global opt plot

--- plots/global_optimization_history.py
+++ plots/global_optimization_history.py
@@ -15,32 +15,39 @@ from mpl_toolkits.axes_grid import inset_locator
 #data_dir = "D:/ClockV5/histories"
 data_dir = "/home/stavros/DATA/MPQ/ClockV5/histories"
 
-name = "allstates1_growglobal"
 machine = ["fullwv", "mpsD4"][1]
-#time_steps_list = [30]
-time_steps = 20
-n_sweeps = 1
-save = False
-
+save = True
 n_sites = 6
+time_steps = 20
 
 
-filename = [name, "nsweeps{}".format(n_sweeps), machine,
-            "N{}M{}.h5".format(n_sites, time_steps)]
+filename = ["allstates1_growglobal", "nsweeps1", machine, "N{}M{}.h5".format(n_sites, time_steps)]
 data = h5py.File(os.path.join(data_dir, "_".join(filename)), "r")
 
 grow = data["_".join(["sweeping", "exact_Eloc"])][()].real.ravel()
 n_grow = len(grow)
 glob = data["exact_Eloc"][()].real
-h_eff = np.concatenate([grow, glob], axis=0)
+heff = np.concatenate([grow, glob], axis=0)
 
 grow = data["_".join(["sweeping", "avg_overlaps"])][()].real.ravel()
 glob = data["avg_overlaps"][()].real
 overlaps = np.concatenate([grow, glob], axis=0)
 data.close()
 
+
+filename = ["allstates1_global", "nsweeps0", machine, "N{}M{}.h5".format(n_sites, time_steps)]
+data = h5py.File(os.path.join(data_dir, "_".join(filename)), "r")
+global_only_heff = data["exact_Eloc"][()].real
+global_only_overlaps = data["avg_overlaps"][()].real
+data.close()
+
+n_global = len(global_only_heff)
+global_only_x = np.arange(n_grow, n_grow + n_global)
+
+
 fig, ax = plt.subplots(figsize=(7, 4))
-ax.semilogy(h_eff, color=cp[0], linewidth=2.4)
+ax.semilogy(heff, color=cp[0], linewidth=2.4)
+ax.semilogy(global_only_x, global_only_heff, color=cp[1], linewidth=2.4)
 ax.axvline(x=n_grow, linestyle="--", linewidth=2.0, color="black")
 plt.xlabel("Epochs")
 plt.ylabel(r"$\left \langle H_\mathrm{eff}\right \rangle $")
@@ -48,18 +55,19 @@ plt.ylabel(r"$\left \langle H_\mathrm{eff}\right \rangle $")
 
 inset_axes = inset_locator.inset_axes(ax, width="50%", height="50%", loc=3)
 plt.semilogy(1 - overlaps, color=cp[0], linewidth=2.4)
+plt.semilogy(global_only_x, 1 - global_only_overlaps, color=cp[1], linewidth=2.4)
 plt.axvline(x=n_grow, linestyle="--", linewidth=2.0, color="black")
 plt.xticks([])
 plt.yticks([])
+inset_axes.xaxis.set_label_coords(0.8,0.185)
+inset_axes.yaxis.set_label_coords(0.16,0.4)
 #plt.xlabel("Epochs")
-#plt.ylabel(r"$1 - \overline{\mathrm{Fid}(t)}$")
+plt.ylabel(r"$1 - \overline{\mathrm{Fid}(t)}$", fontsize=17)
 
 
 if save:
   script_name = __file__.split("/")[-1].split(".")[0]
-  save_name = [script_name, name, machine,
-               "n_sweeps{}".format(n_sweeps),
-               "N{}.png".format(n_sites)]
+  save_name = [script_name, machine, "N{}.pdf".format(n_sites)]
   plt.savefig("_".join(save_name), bbox_inches="tight")
 else:
   plt.show()
