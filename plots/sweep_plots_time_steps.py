82fa9a17d0e63217a87a106e653c746afdc464eb
stavros11 <35475381+stavros11@users.noreply.github.com>
2019-11-26 16:37:43 +0100

    Sweep vs time steps

--- plots/sweep_plots_time_steps.py
+++ plots/sweep_plots_time_steps.py
@@ -11,63 +11,60 @@ matplotlib.rcParams["font.size"] = 26
 cp = sns.color_palette()
 
 
-#data_dir = "D:/ClockV5/histories"
-data_dir = "/home/stavros/DATA/MPQ/ClockV5/histories"
-ylabels = {"sweeping_exact_Eloc": "$E_\mathrm{loc}$",
-           "sweeping_avg_overlaps": r"$1 - \overline{\mathrm{Fid}(t)}$"}
+data_dir = "D:/ClockV5/histories"
+#data_dir = "/home/stavros/DATA/MPQ/ClockV5/histories"
+ylabels = {"exact_Eloc": r"$\left \langle H_\mathrm{eff}\right \rangle $",
+           "avg_overlaps": r"$1 - \overline{\mathrm{Fid}(t)}$"}
 
 
-quantity = ["sweeping_exact_Eloc", "sweeping_avg_overlaps"][1]
+quantity = ["exact_Eloc", "avg_overlaps"][0]
 name = "allstates2"
-machine = ["fullwv", "mpsD4"][0]
-#time_steps_list = [30]
-time_steps_list = [10, 20, 30]
+machine = "fullwv"
+n_sites = 6
+time_steps_list = list(range(10, 31, 5)) + [12, 14, 16, 18]
+time_steps_list.sort()
 n_sweeps = 10
 
 skip_grow = True
-save = False
-
-n_sites = 6
+save = True
 
 
-def get_filedir(time_steps: int) -> str:
-  filename = [name,
-              "nsweeps{}".format(n_sweeps),
-              machine,
-              "N{}M{}.h5".format(n_sites, time_steps)]
-  return os.path.join(data_dir, "_".join(filename))
-
-def transform_plot_data(x: np.ndarray) -> np.ndarray:
-  time_steps = len(x) // n_sweeps
-  if skip_grow:
-    x = x[time_steps:]
-  if "overlap" in quantity:
-    return 1 - x
-  return x
-
-#data = h5py.File(get_filedir(n_sweeps, n_sites, time_steps), "r")
-#for k, v in data.items():
-#  print(k, v.shape)
-
-plt.figure(figsize=(7, 4))
-for time_steps, color in zip(time_steps_list, cp):
-  data = h5py.File(get_filedir(time_steps), "r")
-
-  x_values = np.linspace(0, n_sweeps, n_sweeps * time_steps)
-  xticks = list(range(n_sweeps + 1))
-  if skip_grow:
-    xticks = xticks[1:]
-    x_values = x_values[time_steps:]
-
-  plt.semilogy(x_values, transform_plot_data(data[quantity][()][:, -1].real),
-               label="$T={}$".format(time_steps), color=color, linewidth=2.4)
+quantities = list(ylabels.keys())
+glob = {q: [] for q in quantities}
+grow = {q: [] for q in quantities}
+sweep = {q: [] for q in quantities}
+for time_step in time_steps_list:
+  filename = ["allstates1_binary", "nsweeps{}".format(n_sweeps),
+              machine, "N{}M{}.h5".format(n_sites, time_step)]
+  data = h5py.File(os.path.join(data_dir, "_".join(filename)))
+  for q in quantities:
+    grow[q].append(data["sweeping_{}".format(q)][time_step - 1, -1])
+    sweep[q].append(data["sweeping_{}".format(q)][-1, -1])
   data.close()
 
-plt.axvline(x=1.0, color="black", linestyle="--", linewidth=2.0)
-plt.xlabel("Sweep")
-plt.xticks(xticks)
+  filename = ["allstates1_global",
+              machine, "N{}M{}.h5".format(n_sites, time_step)]
+  data = h5py.File(os.path.join(data_dir, "_".join(filename)))
+  for q in quantities:
+    glob[q].append(data[q][-1])
+  data.close()
+
+glob["avg_overlaps"] = 1 - np.array(glob["avg_overlaps"])
+grow["avg_overlaps"] = 1 - np.array(grow["avg_overlaps"])
+sweep["avg_overlaps"] = 1 - np.array(sweep["avg_overlaps"])
+
+
+plt.figure(figsize=(7, 4))
+plt.semilogy(time_steps_list, glob[quantity], color=cp[0], linewidth=2.6, marker="o",
+             markersize=8, label="Global")
+plt.semilogy(time_steps_list, grow[quantity], color=cp[2], linewidth=2.6, marker="^",
+             markersize=8, label="Grow")
+plt.semilogy(time_steps_list, sweep[quantity], color=cp[1], linewidth=2.6, marker="v",
+             markersize=8, label="Sweep", linestyle=":")
+plt.xlabel("$T$")
 plt.ylabel(ylabels[quantity])
-plt.legend(bbox_to_anchor=(1.0, 1.0), fontsize=22)
+#plt.legend(bbox_to_anchor=(1.0, 1.0), fontsize=22)
+plt.legend(fontsize=20)
 
 
 if save:
