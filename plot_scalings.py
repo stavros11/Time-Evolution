916c26f1743a92852cb0216202211ff0d909fa70
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2020-01-17 20:00:29 +0200

    Log log plot of scaling

--- plot_scalings.py
+++ plot_scalings.py
@@ -11,7 +11,9 @@ matplotlib.rcParams["font.size"] = 20
 cp = sns.color_palette()
 
 data_dir = "/home/stavros/DATA/MPQ/scalings"
-n_sites = 4
+n_sites = 6
+save = True
+run_name = "run2"
 t_final = 1.0
 h_init, h_ev = 1.0, 0.5
 
@@ -19,7 +21,7 @@ filename = ["ed",
             "N{}".format(n_sites),
             "tf{}".format(t_final),
             "h{}_{}".format(h_init, h_ev),
-            "run5"]
+            run_name]
 filename = "{}.h5".format("_".join(filename))
 file = h5py.File(os.path.join(data_dir, filename), "r")
 
@@ -30,20 +32,26 @@ gs_energies = [file["T{}_eigvals".format(time_steps)][()][0]
 print(scipy.stats.linregress(np.log(dt_list), np.log(gs_energies)))
 
 # Plots
-scalings = [2, 3, 4, 5]
+scalings = [3, 4, 5]
 dt_list_exact = np.linspace(dt_list[-1], dt_list[0], 100)
 plt.figure(figsize=(7, 4))
-plt.semilogy(dt_list, gs_energies, color=cp[0], linewidth=2.0, marker="o",
-             markersize=6, label="Ground state")
+plt.loglog(dt_list, gs_energies, color=cp[0], linewidth=2.0, marker="o",
+           markersize=6, label="Ground state")
 
 for exponent in scalings:
   scaling = (dt_list_exact**exponent * gs_energies[-1] /
              dt_list_exact[0]**exponent)
-  plt.semilogy(dt_list_exact, scaling, color=cp[exponent],
+  plt.semilogy(dt_list_exact, scaling, color=cp[exponent - 1],
                linewidth=2.0, linestyle="--",
                label=r"$\sim \delta t^{}$".format(exponent))
 
 plt.xlabel(r"$\delta t$")
 plt.ylabel(r"$\left \langle H_\mathrm{eff}\right \rangle $")
 plt.legend()
-plt.show()
+
+if save:
+  script_name = __file__.split("/")[-1].split(".")[0]
+  save_name = [script_name, run_name, "N{}.pdf".format(n_sites)]
+  plt.savefig("_".join(save_name), bbox_inches="tight")
+else:
+  plt.show()
