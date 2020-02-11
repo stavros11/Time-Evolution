b77a36e69d2528dd83d4fc330dbb19c61c1e8a97
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2020-02-11 20:00:17 +0400

    Implement growing in time

--- machines/base.py
+++ machines/base.py
@@ -1,17 +1,36 @@
 import numpy as np
 import tensorflow as tf
+from typing import Any, Callable
 
 
 class Base:
 
-  def __init__(self, dtype=tf.float64, learning_rate: float = 1e-3):
+  def __init__(self, initial_condition: np.ndarray,
+               time_steps: int,
+               dtype: Any = tf.float64,
+               learning_rate: float = 1e-3):
     self.rtype = dtype
+    self.time_steps = time_steps
+    self.initial_condition = initial_condition
+    self.init_params = {"initial_condition": initial_condition,
+                        "time_steps": time_steps,
+                        "dtype": dtype,
+                        "learning_rate": learning_rate}
+
     self._dense = None
     self.mask = None
+
     self.optimizer = tf.keras.optimizers.Adam(learning_rate)
 
+  def __call__(self) -> tf.Tensor:
+    raise NotImplementedError
+
+  @classmethod
+  def add_time_step(cls, old_machine: "Base") -> "Base":
+    raise NotImplementedError
+
   @property
-  def ctype(self):
+  def ctype(self) -> Any:
     if self.rtype == tf.float32:
       return tf.complex64
     elif self.rtype == tf.float64:
@@ -20,12 +39,12 @@ class Base:
       raise TypeError
 
   @property
-  def dense(self):
+  def dense(self) -> tf.Tensor:
     if self._dense is None:
       self._dense = self.__call__()
     return self._dense
 
-  def update(self, objective) -> tf.Tensor:
+  def update(self, objective: Callable[[tf.Tensor], tf.Tensor]) -> tf.Tensor:
     with tf.GradientTape() as tape:
       self._dense = self.__call__()
       loss = objective(self._dense)
@@ -39,9 +58,15 @@ class Base:
 
 class FullWavefunction(Base):
 
-  def __init__(self, initial_state: np.ndarray, dtype=tf.float64,
+  def __init__(self, initial_condition: np.ndarray,
+               time_steps: int,
+               dtype: Any = tf.float64,
                learning_rate: float = 1e-3):
-    super(FullWavefunction, self).__init__(dtype, learning_rate)
+    super(FullWavefunction, self).__init__(initial_condition,
+                                           time_steps,
+                                           dtype,
+                                           learning_rate)
+    initial_state = np.array((self.time_steps + 1) * [initial_condition])
     self.var_re = tf.Variable(initial_state.real, dtype=self.rtype)
     self.var_im = tf.Variable(initial_state.imag, dtype=self.rtype)
     self.variables = [self.var_re, self.var_im]
@@ -53,24 +78,47 @@ class FullWavefunction(Base):
   def __call__(self) -> tf.Tensor:
     return tf.complex(self.var_re, self.var_im)
 
+  @classmethod
+  def add_time_step(cls, old_machine: "FullWavefunction") -> "FullWavefunction":
+    params = dict(old_machine.init_params)
+    params["time_steps"] += 1
+    new_machine = cls(**params)
+
+    new_var_re = tf.concat([old_machine.var_re,
+                            old_machine.var_re[-1][tf.newaxis]], axis=0)
+    new_machine.var_re.assign(new_var_re)
+    new_var_im = tf.concat([old_machine.var_im,
+                            old_machine.var_im[-1][tf.newaxis]], axis=0)
+    new_machine.var_im.assign(new_var_im)
+
+    return new_machine
+
 
 class FullProp(Base):
 
-  def __init__(self, initial_state: np.ndarray, dtype=tf.float64,
+  def __init__(self,
+               initial_condition: np.ndarray,
+               time_steps: int,
+               dtype: Any = tf.float64,
                learning_rate: float = 1e-3):
-    super(FullProp, self).__init__(dtype, learning_rate)
-    self.time_steps, n_states = initial_state.shape
-    self.time_steps -= 1
+    super(FullProp, self).__init__(initial_condition, time_steps,
+                                   dtype, learning_rate)
+    n_states = len(self.initial_condition)
 
     self.u_re = tf.Variable(np.eye(n_states), dtype=self.rtype)
     self.u_im = tf.Variable(np.eye(n_states), dtype=self.rtype)
     self.variables = [self.u_re, self.u_im]
 
-    self.psi0 = tf.convert_to_tensor(initial_state[0])
+    self.psi0 = tf.convert_to_tensor(initial_condition, dtype=self.ctype)
 
-  def __call__(self):
+  def __call__(self) -> tf.Tensor:
     states = [self.psi0[:, tf.newaxis]]
     u = tf.complex(self.u_re, self.u_im)
     for _ in range(self.time_steps):
       states.append(tf.matmul(u, states[-1]))
     return tf.stack(states)[:, :, 0]
+
+  @classmethod
+  def add_time_step(cls, old_machine: "FullProp") -> "FullProp":
+    old_machine.time_steps += 1
+    return old_machine
--- main_grow.py
+++ main_grow.py
@@ -10,8 +10,9 @@ time_steps = 20
 t_final = 1.0
 h_init = 1.0
 h_ev = 0.5
-n_epochs = 20000
+n_epochs = 1000
 n_message = 500
+machine_type = "FullWavefunction"
 
 dt = t_final / time_steps
 t = np.linspace(0, t_final, time_steps + 1)
@@ -23,24 +24,25 @@ exact_state, exact_obs = tfim.tfim_exact_evolution(n_sites, t_final, time_steps,
                                                    h0=h_init, h=h_ev)
 
 # Define ansatz
-model = machines.FullWavefunction(exact_state, time_steps, tf.float32)
+model = getattr(machines, machine_type)(exact_state[0], 1, tf.float32)
 
 ham_tf = tf.convert_to_tensor(ham, dtype=model.ctype)
 ham2_tf = tf.cast(tf.matmul(ham, ham), dtype=model.ctype)
-print(model.ctype)
-print(ham_tf.dtype)
-print(ham2_tf.dtype)
 objective_func = functools.partial(clock.energy, Ham=ham_tf, dt=dt, Ham2=ham2_tf)
 
 
 # Optimization
 history = {"exact_Eloc": [], "avg_overlaps": []}
-for epoch in range(n_epochs):
-  energy = model.update(objective_func)
-  history["exact_Eloc"].append(energy.numpy())
-  history["avg_overlaps"].append(
-        calc.averaged_overlap(model.dense.numpy(), exact_state))
-  if epoch % n_message == 0:
-    print("\nEpoch: {}".format(epoch))
-    for k, v in history.items():
-      print("{}: {}".format(k, v[-1]))
+for it in range(1, time_steps + 1):
+  for epoch in range(n_epochs):
+    energy = model.update(objective_func)
+    history["exact_Eloc"].append(energy.numpy())
+    history["avg_overlaps"].append(
+          calc.averaged_overlap(model.dense.numpy(), exact_state[:it + 1]))
+
+  print("\nTime step: {}".format(it))
+  for k, v in history.items():
+    print("{}: {}".format(k, v[-1]))
+
+  if it < time_steps:
+    model = getattr(machines, machine_type).add_time_step(model)
