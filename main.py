64219c1bb4825d7b78c2fa9ed4f0a62fd638a4d9
stavros11 <stavrosefthm@gmail.com>
2020-02-16 10:11:11 +0400

    Merge grow with old main

--- main.py
+++ main.py
@@ -31,12 +31,14 @@ parser.add_argument("--n-epochs", default=0, type=int,
                     help="Number of epochs to train for.")
 parser.add_argument("--n-message", default=500, type=int,
                     help="Every how many epochs to display messages.")
-
+parser.add_argument("--grow", action="store_true",
+                    help="Whether to grow in time.")
 
 
 def main(n_sites: int, time_steps: int, t_final: float, h_init: float,
          h_ev: float, n_epochs: int, n_message: int,
-         machine_type: str, n_hidden: Optional[int] = None):
+         machine_type: str, n_hidden: Optional[int] = None,
+         grow: bool = False):
   dt = t_final / time_steps
   t = np.linspace(0, t_final, time_steps + 1)
   assert dt == t[1] - t[0]
@@ -53,6 +55,8 @@ def main(n_sites: int, time_steps: int, t_final: float, h_init: float,
                     "dtype": tf.float32}
   if n_hidden is not None:
     machine_params["n_hidden"] = n_hidden
+  if grow:
+    machine_params["time_steps"] = 1
   model = getattr(machines, machine_type)(**machine_params)
 
   ham_tf = tf.convert_to_tensor(ham, dtype=model.ctype)
@@ -62,16 +66,34 @@ def main(n_sites: int, time_steps: int, t_final: float, h_init: float,
 
   # Optimization
   history = {"exact_Eloc": [], "avg_overlaps": []}
-  for epoch in range(n_epochs):
-    energy = model.update(objective_func)
-    history["exact_Eloc"].append(energy.numpy())
-    history["avg_overlaps"].append(
-          calc.averaged_overlap(model.dense.numpy(), exact_state))
-    if epoch % n_message == 0:
-      print("\nEpoch: {}".format(epoch))
+  if grow:
+    for it in range(1, time_steps + 1):
+      for epoch in range(n_epochs):
+        energy = model.update(objective_func)
+        history["exact_Eloc"].append(energy.numpy())
+        history["avg_overlaps"].append(
+              calc.averaged_overlap(model.dense.numpy(), exact_state[:it + 1]))
+
+      # Print messages
+      print("\nTime step: {}".format(it))
       for k, v in history.items():
         print("{}: {}".format(k, v[-1]))
 
+      if it < time_steps:
+        model = getattr(machines, machine_type).add_time_step(model)
+
+  else:
+    for epoch in range(n_epochs):
+      energy = model.update(objective_func)
+      history["exact_Eloc"].append(energy.numpy())
+      history["avg_overlaps"].append(
+            calc.averaged_overlap(model.dense.numpy(), exact_state))
+
+      # Print messages
+      if epoch % n_message == 0:
+        print("\nEpoch: {}".format(epoch))
+        for k, v in history.items():
+          print("{}: {}".format(k, v[-1]))
 
 
 if __name__ == '__main__':
